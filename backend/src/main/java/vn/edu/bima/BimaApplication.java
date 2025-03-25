3c6ae72f661b8c563c09818edb367cf9fd957d92
Sukkaito <40479420+Sukkaito@users.noreply.github.com>
2025-03-25 20:37:34 +0700

    Init Gradle

---
+++ backend/src/main/java/vn/edu/bima/BimaApplication.java
@@ -0,0 +1,12 @@
+package vn.edu.bima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimaApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/vn/edu/bima/BimaApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.bima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
